98369e279acecb7be87cbc37b124b9be36a1f458
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-07 17:47:52 +0900

    Feat: 5번 문제(1992)

--- Baekjoon/week2/1992/quadTree.js
+++ Baekjoon/week2/1992/quadTree.js
@@ -1,1 +1,35 @@
 // 5번 문제: 쿼드트리(quadTree.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+const [N, ...arr] = input;
+const area = arr.map((item) => item.split("").map(Number));
+
+const quadTree = [];
+
+function recursion(n, x, y) {
+  let total = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      total += area[y + j][x + i];
+    }
+  }
+  if (total === 0) quadTree.push(0);
+  else if (total === n * n) quadTree.push(1);
+  else {
+    n /= 2;
+    quadTree.push("(");
+    recursion(n, x, y);
+    recursion(n, x + n, y);
+    recursion(n, x, y + n);
+    recursion(n, x + n, y + n);
+    quadTree.push(")");
+  }
+}
+
+recursion(N, 0, 0);
+console.log(quadTree.join(""));
