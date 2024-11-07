c1abfb1d5fb6d624611740bde5626e0eb1654b8b
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-07 22:01:53 +0900

    Feat: 6번 문제(2828)

--- Baekjoon/week2/2828/appleGame.js
+++ Baekjoon/week2/2828/appleGame.js
@@ -1,1 +1,29 @@
 // 6번 문제: 사과 담기 게임(appleGame.js)
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+input.shift();
+const apples = input.map(Number);
+
+let left = 1;
+let right = M;
+let move = 0;
+
+for (let i = 0; i < apples.length; i++) {
+  const apple = apples[i];
+  if (apple >= left && apple <= right) continue;
+  else if (apple < left) {
+    const dist = left - apple;
+    left = apple;
+    right = right - dist;
+    move += dist;
+  } else {
+    apple > right;
+    const dist = apple - right;
+    right = apple;
+    left = left + dist;
+    move += dist;
+  }
+}
+console.log(move);
