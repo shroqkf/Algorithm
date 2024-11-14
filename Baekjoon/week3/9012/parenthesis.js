aa913b708cba6a0ce6a7346b6b6cdc8418d4666c
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-14 16:26:35 +0900

    Feat: 4번 문제(9012)

--- Baekjoon/week3/9012/parenthesis.js
+++ Baekjoon/week3/9012/parenthesis.js
@@ -1,1 +1,27 @@
 // 4번 문제: 괄호(parenthesis.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+[T, ...strings] = input;
+T = Number(T);
+VPS(T, strings);
+
+function VPS(T, strings) {
+  for (let i = 0; i < T; i++) {
+    const string = strings[i];
+    const stack = [];
+    let result = "YES";
+
+    for (let j = 0; j < string.length; j++) {
+      if (string[j] === "(") stack.push(string[j]);
+      else if (string[j] === ")") {
+        if (!stack.pop()) {
+          result = "NO";
+          break;
+        }
+      }
+    }
+    if (stack.length !== 0) result = "NO";
+    console.log(result);
+  }
+}
