4834cdf7ef64b68a8ec01a9fb541033d4e3034fc
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-14 16:55:47 +0900

    Feat: 1번 문제(3474)

--- Baekjoon/week3/3474/Professor.js
+++ Baekjoon/week3/3474/Professor.js
@@ -1,1 +1,23 @@
 // 1번 문제: 교수가 된 현우(Professor.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [T, ...N] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+function TSP(num) {
+  let count = 0;
+  for (let i = 5; i <= num; i *= 5) {
+    count += Math.floor(num / i);
+  }
+  return count;
+}
+
+let answer = "";
+for (let i = 0; i < T; i++) {
+  answer += TSP(N[i]) + "\n";
+}
+
+console.log(answer.trim());
