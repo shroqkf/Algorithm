eaf96e5a74d49fa869e642feef2d229c023847d0
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-19 21:27:22 +0900

    feat: 3번 문제(3986)

--- Baekjoon/week4/3986/goodWord.js
+++ Baekjoon/week4/3986/goodWord.js
@@ -1,1 +1,27 @@
 // 3번 문제: 좋은 단어(goodWord.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.trim());
+const [N, ...words] = input;
+
+function solution(N, words) {
+  let answer = 0;
+
+  words.map((v) => {
+    let arr = [];
+    v = v.split("");
+
+    for (let word of v) {
+      if (arr[arr.length - 1] === word) arr.pop();
+      else arr.push(word);
+    }
+    if (arr.length === 0) answer++;
+  });
+  return answer;
+}
+
+console.log(solution(N, words));
