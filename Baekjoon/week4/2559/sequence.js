f9594c5370411ca8d1c3b1303bdb8b5fe870a33a
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-11-21 12:06:42 +0900

    feat: 1번 문제(2559)

--- Baekjoon/week4/2559/sequence.js
+++ Baekjoon/week4/2559/sequence.js
@@ -1,1 +1,23 @@
 // 1번 문제: 수열(sequence.js)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [NK, num] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map(Number));
+
+const [N, K] = NK;
+let sum = 0;
+const answer = [];
+
+for (let i = 0; i < K; i++) {
+  sum += num[i];
+}
+answer.push(sum);
+
+for (let i = K; i < N; i++) {
+  sum = sum - num[i - K] + num[i];
+  answer.push(sum);
+}
+console.log(Math.max(...answer));
